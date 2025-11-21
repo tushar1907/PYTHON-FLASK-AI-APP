245866445dcfbb55ee4b4857bf192abfba56078f
Gupta <tushar_19@hotmail.com>
2025-11-21 10:48:46 -0800

    Hands on Lab 3

---
+++ handsonLab3/myMath/basic.py
@@ -0,0 +1,25 @@
+def square(number):
+    """
+    This function returns the square of a given number
+    """
+    return number ** 2
+
+def double(number):
+    """
+    This function returns twice the value of a given number
+    """
+    return number * 2
+
+def add(a, b):
+    """
+    This function returns the sum of given numbers
+    """
+    return a + b
+
+def area_of_rectangle(l, b):
+
+    return l * b
+
+def area_of_circle(r):
+
+    return 3.17 * r * r
